ad5119dfd6a3a84d0ae5135be785423fd5b16f54
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2024-06-18 08:32:32 +0300

    Formatting & javadocs for SwerveDriveCommand

--- src/main/java/frc/robot/commands/SwerveDriveCommand.java
+++ src/main/java/frc/robot/commands/SwerveDriveCommand.java
@@ -27,6 +27,20 @@ public class SwerveDriveCommand extends Command {
   private final BooleanSupplier fieldCentric;
   private final boolean squareInputs;
 
+  /**
+   * Create a new command to drive a serve robot.
+   * 
+   * @param driveBase        The {@link DriveBase} to drive.
+   * @param strafeXSupplier  Supplier for strafe in X direction, e.g. from a
+   *                         joystick.
+   * @param strafeYSupplier  Supplier for strafe in Y direction, e.g. from a
+   *                         joystick.
+   * @param rotationSupplier Supplier for rotation, e.g. from a joystick.
+   * @param fieldCentric     Supplier for whether or not to drive field centric.
+   * @param squareInputs     If true, square the linear magnitude of the strafe
+   *                         suppliers combined and that of the rotation, but
+   *                         keeps direction the same.
+   */
   public SwerveDriveCommand(DriveBase driveBase, DoubleSupplier strafeXSupplier, DoubleSupplier strafeYSupplier,
       DoubleSupplier rotationSupplier, BooleanSupplier fieldCentric, boolean squareInputs) {
     this.driveBase = driveBase;
@@ -38,8 +52,20 @@ public class SwerveDriveCommand extends Command {
     addRequirements(driveBase);
   }
 
-  public SwerveDriveCommand(DriveBase driveBase, double strafeX, double strafeY, double rotation,
-      boolean fieldCentric, boolean squareInputs) {
+  /**
+   * Create a new command to drive a serve robot.
+   * 
+   * @param driveBase        The {@link DriveBase} to drive.
+   * @param strafeXSupplier  Constant strafe in X direction, e.g. from a joystick.
+   * @param strafeYSupplier  Constant strafe in Y direction, e.g. from a joystick.
+   * @param rotationSupplier Constant rotation, e.g. from a joystick.
+   * @param fieldCentric     Whether or not to drive field centric.
+   * @param squareInputs     If true, square the linear magnitude of the strafe
+   *                         suppliers combined and that of the rotation, but
+   *                         keeps direction the same.
+   */
+  public SwerveDriveCommand(DriveBase driveBase, double strafeX, double strafeY, double rotation, boolean fieldCentric,
+      boolean squareInputs) {
     this(driveBase, () -> strafeX, () -> strafeY, () -> rotation, () -> fieldCentric, squareInputs);
   }
 
@@ -52,15 +78,14 @@ public class SwerveDriveCommand extends Command {
       double omega = MathUtil.applyDeadband(rotationSupplier.getAsDouble(), IOConstants.JOYSTICK_DEADBAND);
 
       // Square values
-      if(squareInputs) {
+      if (squareInputs) {
         linearMagnitude = linearMagnitude * linearMagnitude;
         omega = Math.copySign(omega * omega, omega);
-      } 
+      }
 
       // Calculate new linear velocity
       Translation2d linearVelocity = new Pose2d(new Translation2d(), linearDirection)
-          .transformBy(new Transform2d(linearMagnitude, 0.0, new Rotation2d()))
-          .getTranslation();
+          .transformBy(new Transform2d(linearMagnitude, 0.0, new Rotation2d())).getTranslation();
 
       driveBase.driveRobotRelative(
           ChassisSpeeds.fromFieldRelativeSpeeds(linearVelocity.getX() * DriveConstants.MAX_DRIVE_SPEED,
